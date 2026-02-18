5d982a5f15cadab7f4461169022649bfba217f50
ragul763939 <rr0741130@gmail.com>
2026-02-18 10:39:56 +0530

    UC1

--- src/PalindromeCheckerApp/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/PalindromeCheckerApp.java
@@ -1,7 +1,21 @@
 package PalindromeCheckerApp;
 
 public class PalindromeCheckerApp {
-   public  static void main(String[] args) {
-       System.out.println("Welcome Palindrome");
+
+    /**
+     * Use Case 1: Application Entry & Welcome Message
+     * Description:
+     * - Starts execution from main()
+     * - Displays a welcome message
+     * - Shows application version
+     * - Confirms system initialization
+     *
+     * Version: 1.0
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
