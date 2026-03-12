7986f813e74fea62028dbc926a7aa056dd439a20
Ragul Mahendran <rr0741130@gmail.com>
2026-03-12 09:02:07 +0530

    Implement palindrome checker functionality
    Added a new class for palindrome checking with case and space insensitivity.

--- src/PalindromeCheckerApp/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/PalindromeCheckerApp.java
@@ -1,7 +1,43 @@
-package PalindromeCheckerApp;
+package palindromecheckerapp;
 
-public class PalindromeCheckerApp {
-   public  static void main(String[] args) {
-       System.out.println("Welcome Palindrome");
+import java.util.Scanner;
+
+public class UseCase10PalindromeCheckerApp {
+
+    // Function to check palindrome ignoring case and spaces
+    public static boolean isPalindrome(String str) {
+
+        // Normalize string: remove spaces and convert to lowercase
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        // Check characters from both ends
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The given string is a Palindrome (ignoring spaces and case).");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }
