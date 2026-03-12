abfe826b721027aac402bbf69c72101548ea6fa1
Ragul Mahendran <rr0741130@gmail.com>
2026-03-12 09:04:15 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/PalindromeCheckerApp.java
@@ -1,7 +1,49 @@
-package PalindromeCheckerApp;
+package palindromecheckerapp;
 
-public class PalindromeCheckerApp {
-   public  static void main(String[] args) {
-       System.out.println("Welcome Palindrome");
+import java.util.Scanner;
+
+// Service class that contains palindrome logic
+class PalindromeChecker {
+
+    // Method to check palindrome
+    public boolean checkPalindrome(String str) {
+
+        // Normalize string (ignore spaces and case)
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Create object of PalindromeChecker
+        PalindromeChecker checker = new PalindromeChecker();
+
+        if (checker.checkPalindrome(input)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }
