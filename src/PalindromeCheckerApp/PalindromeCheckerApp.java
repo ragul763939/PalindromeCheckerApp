f7ef9988ad4091b9451f2ee30791a03d706018b6
Ragul Mahendran <rr0741130@gmail.com>
2026-03-12 09:54:51 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp/PalindromeCheckerApp.java
@@ -1,35 +1,82 @@
-import java.util.Stack;
+package palindromecheckerapp;
 
-public class UseCase5PalindromeCheckerApp {
+import java.util.*;
+
+public class UseCase13PalindromeCheckerApp {
+
+    // Method 1: Two Pointer Approach
+    public static boolean twoPointerCheck(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    // Method 2: Stack Approach
+    public static boolean stackCheck(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Method 3: Reverse String Approach
+    public static boolean reverseCheck(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        return str.equals(reversed);
+    }
 
     public static void main(String[] args) {
 
-        // Original String
-        String originalString = "madam";
+        Scanner sc = new Scanner(System.in);
 
-        // Create Stack of Characters
-        Stack<Character> stack = new Stack<>();
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
 
-        // Step 1: Push each character into stack
-        for (int i = 0; i < originalString.length(); i++) {
-            stack.push(originalString.charAt(i));
-        }
+        // Two Pointer Timing
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(input);
+        long end1 = System.nanoTime();
 
-        // Step 2: Pop characters and build reversed string
-        String reversedString = "";
+        // Stack Timing
+        long start2 = System.nanoTime();
+        boolean result2 = stackCheck(input);
+        long end2 = System.nanoTime();
 
-        while (!stack.isEmpty()) {
-            reversedString = reversedString + stack.pop();
-        }
+        // Reverse String Timing
+        long start3 = System.nanoTime();
+        boolean result3 = reverseCheck(input);
+        long end3 = System.nanoTime();
 
-        // Step 3: Compare original and reversed string
-        if (originalString.equals(reversedString)) {
-            System.out.println("The given string \"" + originalString + "\" is a Palindrome.");
-        } else {
-            System.out.println("The given string \"" + originalString + "\" is NOT a Palindrome.");
-        }
+        System.out.println("\nPalindrome Results:");
+        System.out.println("Two Pointer: " + result1 + " | Time: " + (end1 - start1) + " ns");
+        System.out.println("Stack: " + result2 + " | Time: " + (end2 - start2) + " ns");
+        System.out.println("Reverse String: " + result3 + " | Time: " + (end3 - start3) + " ns");
 
-        // Program exits
+        sc.close();
     }
 }
-
